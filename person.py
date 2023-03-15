48af14fed7a86bbd03c865adcacca4c8835c58e3
Christian Prather <clemsbach@gmail.com>
2023-03-15 10:15:04 -0600

    Scaled and added random start position

--- person.py
+++ person.py
@@ -2,20 +2,26 @@ from location import Location
 import random
 from ursina import Entity, time
 
+
 def pick_random_walk():
     return random.randint(-1, 1)
 
 
 class Person(Entity):
-    print("Generating Person")
-    start_location = Location(
-        random.randint(0, 100), random.randint(0, 100))
-    current_location = start_location
-    lifetime = random.randint(0, 2000)
-    walk_time = 0
-    walk_direction = pick_random_walk()
-    horizontal = True
-    walk_length = random.randint(30, 55)
+    def __init__(self, add_to_scene_entities=True, **kwargs):
+        super().__init__(add_to_scene_entities, **kwargs)
+        print("Generating Person")
+        self.start_location = Location(
+            random.randint(0, 1), random.randint(0, 1))
+        self.current_location = self.start_location
+
+        self.lifetime = random.randint(0, 2000)
+        self.walk_time = 0
+        self.walk_direction = pick_random_walk()
+        self.horizontal = True
+        self.walk_length = random.randint(30, 55)
+        self.x = self.start_location.x
+        self.y = self.start_location.y
 
     def update(self):
         '''
@@ -31,12 +37,12 @@ class Person(Entity):
                 self.horizontal = False
         else:
             if self.walk_time < self.walk_length:
-                    self.y += self.walk_direction * time.dt
-                    self.walk_time += 1
+                self.y += self.walk_direction * time.dt
+                self.walk_time += 1
             else:
                 self.walk_direction = pick_random_walk()
-                self.walk_time = 0 
-                self.horizontal = True   
+                self.walk_time = 0
+                self.horizontal = True
 
     def update_position(self):
         '''
--- sim.py
+++ sim.py
@@ -3,10 +3,10 @@ from person import Person
 
 app = Ursina()
 
-cart_1 = Person(model="quad", color=color.blue)
-cart_2 = Person(model="quad", color=color.red)
+cart_1 = Person(model="quad", color=color.blue, scale=(0.3, 0.3, 0.3))
+cart_2 = Person(model="quad", color=color.red, scale=(0.3, 0.3, 0.3))
 
-cart_3 = Person(model="quad", color=color.pink)
-cart_4 = Person(model="quad", color=color.white)
+cart_3 = Person(model="quad", color=color.pink, scale=(0.3, 0.3, 0.3))
+cart_4 = Person(model="quad", color=color.white, scale=(0.3, 0.3, 0.3))
 
 app.run()
