c5adb86072c050e558d68f8b7e92d1718a75495a
Christian Prather <clemsbach@gmail.com>
2023-03-15 19:05:27 -0600

    Added kiosks

--- sim_ursina.py
+++ sim_ursina.py
@@ -1,13 +1,16 @@
 from ursina import *
 from ursina.prefabs.tilemap import Tilemap
 from person import Person
+from kiosk import Kiosk
 import pandas as pd
 import pytmx
 
 START_PEOPLE = 15
 
+
 active_people = []
 # walls = []
+kiosks = {}
 
 
 def gen_world():
@@ -17,9 +20,15 @@ def gen_world():
     layer = tmxdata.get_layer_by_name('walls')
     for x, y, surf in layer.tiles():
         wall = Entity(model="quad", x=x-15, y=-y + 10,
-                        scale=(1, 1, 1), collider="box")
+                      scale=(1, 1, 1), collider="box")
         # print(x, y)
         # walls.append(wall)
+
+    layer = tmxdata.get_layer_by_name('kiosks')
+    for x, y, surf in layer.tiles():
+        kiosk = Kiosk(model="quad", x=x-15, y=-y + 10,
+                       scale=(1, 1, 1), collider="box", color=color.blue)
+        kiosks[kiosk.__hash__] = (kiosk.x, kiosk.y)
 
     print(layer.data)
 
@@ -28,27 +37,30 @@ def gen_world():
         active_people.append(person)
 
 
+def people_lifetime(life_counter):
+
+    life_counter[0] += 1 * time.dt
+    if life_counter[0] > 20:
+        index = random.randint(0, len(active_people)-1)
+        # print(index)
+        person = active_people.pop(index)
+        person.disable()
+
+        life_counter[0] = 0
+
 
 def main():
     app = Ursina()
 
     camera.orthographic = True
-    life_counter = 0
 
     gen_world()
-    print(len(active_people))
-    
+    print(len(kiosks))
+    # print(len(active_people))
+    life_counter = [0]
 
     while True:
-        life_counter += 1 * time.dt
-        if life_counter >10:
-            index = random.randint(0,len(active_people)-1)
-            # print(index)
-            person = active_people.pop(index)
-            person.disable()
-
-            life_counter = 0
-            
+        people_lifetime(life_counter=life_counter)
         app.step()
     # app.run()
 
